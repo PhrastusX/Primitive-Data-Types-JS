283193569bd3781bff5c13bbc400d1b51a42acdf
PhrastusX <56659535+PhrastusX@users.noreply.github.com>
2020-01-19 12:33:38 -0700

    finished problem set

---
+++ problemSet.js
@@ -0,0 +1,55 @@
+// Write your first name as a string and your last name as a string
+"Theo"
+"Diehl"
+
+// Now, print your first name and last name separately using Javascript’s console.log method. More information can be found here: https://developer.mozilla.org/en-US/docs/Web/API/Console/log. Try this with single quotes and double quotes.
+
+console.log("Theo")
+console.log("Diehl")
+
+// Now, print your first and last name out in the same line of code using concatenation
+
+console.log("Theo" + " Diehl")
+
+// In written form, explain what a string is and what every string requires
+/* A sting is a data type in JS.  Every string requires either "" or '' or `` around it */
+
+// Integer and Float Practice
+
+
+// In written form, explain what an integer is and what a float is. How do they differ?
+/* An int is a data type that does not have a decimal point. A float is a data type that has a decimal point. */
+
+
+// Using the Javascript console, practice mathematical operations including
+
+// – Addition, Subtraction, Multiplication, Division
+
+console.log( 1 +1 , 2-3, 2 *3, 4/2 )
+
+// – Modulus
+
+console.log( 5 % 2) //output will be 1 gives back the remainder.
+
+// – Incrementing and Decrementing 
+let i = 0
+i += 1
+console.log(i) //should be 1
+
+let x = 1
+x -= 1
+console.log(x) // should be 0
+
+// – Exponents
+
+let e = 2 **2
+console.log(e) // should be 4
+
+// Next, practice solving mathematical problems using the order of operations
+
+const pi = 3.14
+
+
+let area = pi*1**2
+
+console.log(area)//prints out area of a circle
