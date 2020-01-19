ee7c8e3d1be0f955e41a58b9b9f8e63a25b761a4
PhrastusX <56659535+PhrastusX@users.noreply.github.com>
2020-01-19 11:23:13 -0700

    added operations/ ints and floats

--- PDT.js
+++ PDT.js
@@ -1,3 +1,19 @@
 //Strings
 //Can be single or double quotes ' ' or " "
 console.log("Hello World", 'Hello World', 'Hello' + ' World');
+
+// operations on ints and floats.
+
+console.log(1 +1)
+console.log(1 -1)
+console.log(1 *1)
+console.log(1 /1)
+console.log(3 %2)
+
+console.log(1.1 +1.1)
+console.log(1.11* 2)
+console.log(1.2 /1.1)
+console.log(1.15-1.10)
+
+
+
