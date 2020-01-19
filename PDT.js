bfdff90a3ee6fb8c9e6c81490534c62ffdd8ad35
PhrastusX <56659535+PhrastusX@users.noreply.github.com>
2020-01-19 11:36:54 -0700

    added back ticks to strings

--- PDT.js
+++ PDT.js
@@ -1,6 +1,6 @@
 //Strings
 //Can be single or double quotes ' ' or " "
-console.log("Hello World", 'Hello World', 'Hello' + ' World');
+console.log("Hello World", 'Hello World', 'Hello' + ' World', `Hello World`);
 
 // operations on ints and floats.
 
